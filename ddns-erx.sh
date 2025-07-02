1276b40862281dc6beee888a61a59a03ffc140df
reidme <55491460+reidmex@users.noreply.github.com>
2025-07-02 17:48:49 +0800

    更新 ddns-erx.sh

--- ddns-erx.sh
+++ ddns-erx.sh
@@ -14,117 +14,116 @@ FORCE_UPDATE=false
 
 
 if [ -n "$BASH_SOURCE" ]; then
-	workDir=$(
-		cd $(dirname "$BASH_SOURCE")
-		pwd
-	)
+        workDir=$(
+                cd $(dirname "$BASH_SOURCE")
+                pwd
+        )
 else
-	workDir=$(
-		cd $(dirname $0)
-		pwd
-	)
+        workDir=$(
+                cd $(dirname $0)
+                pwd
+        )
 fi
 echo "workDir: $workDir"
 cd $workDir
 
 while getopts k:n:t:u:z:i:fr: opts; do
-	case ${opts} in
-		t) CLOUDFLARE_API_TOKEN=${OPTARG} ;;
-		k) CLOUDFLARE_API_KEY=${OPTARG} ;;
-		n) CLOUDFLARE_RECORD_NAME=${OPTARG} ;;
-		r) CLOUDFLARE_RECORD_TYPE=${OPTARG} ;;
-		u) CLOUDFLARE_USER_MAIL=${OPTARG} ;;
-		z) CLOUDFLARE_ZONE_NAME=${OPTARG} ;;
-		i) OUTBOUND_INTERFACE=${OPTARG} ;;
-		f) FORCE_UPDATE=true ;;
-		*);;
-	esac
+        case ${opts} in
+                t) CLOUDFLARE_API_TOKEN=${OPTARG} ;;
+                k) CLOUDFLARE_API_KEY=${OPTARG} ;;
+                n) CLOUDFLARE_RECORD_NAME=${OPTARG} ;;
+                r) CLOUDFLARE_RECORD_TYPE=${OPTARG} ;;
+                u) CLOUDFLARE_USER_MAIL=${OPTARG} ;;
+                z) CLOUDFLARE_ZONE_NAME=${OPTARG} ;;
+                i) OUTBOUND_INTERFACE=${OPTARG} ;;
+                f) FORCE_UPDATE=true ;;
+                *);;
+        esac
 done
 
 if [ "$CLOUDFLARE_API_TOKEN" = "" ] && [ "$CLOUDFLARE_API_KEY" = "" ]; then
-	LOG_TIME=`date`
-	printf "$LOG_TIME: CLOUDFLARE_API_TOKEN or CLOUDFLARE_API_KEY is required.\n"
-	exit 2
+        LOG_TIME=`date`
+        printf "$LOG_TIME: CLOUDFLARE_API_TOKEN or CLOUDFLARE_API_KEY is required.\n"
+        exit 2
 fi
 
 if [ "$CLOUDFLARE_API_TOKEN" != "" ] && [ "$CLOUDFLARE_API_KEY" != "" ]; then
-	LOG_TIME=`date`
-	printf "$LOG_TIME: CLOUDFLARE_API_TOKEN and CLOUDFLARE_API_KEY both set, CLOUDFLARE_API_KEY will be ignored.\n"
+        LOG_TIME=`date`
+        printf "$LOG_TIME: CLOUDFLARE_API_TOKEN and CLOUDFLARE_API_KEY both set, CLOUDFLARE_API_KEY will be ignored.\n"
 fi
 
 if [ "$CLOUDFLARE_RECORD_NAME" = "" ]; then
-	LOG_TIME=`date`
-	printf "$LOG_TIME: CLOUDFLARE_RECORD_NAME is required.\n"
-	exit 2
+        LOG_TIME=`date`
+        printf "$LOG_TIME: CLOUDFLARE_RECORD_NAME is required.\n"
+        exit 2
 fi
 
 PRIMARY_IP_API="https://icanhazip.com"
 BACKUP_IP_API="https://ifconfig.me"
 
 if [ "$CLOUDFLARE_RECORD_TYPE" = "A" ]; then
-	IP_VERSION="-4"
+        IP_VERSION="-4"
 elif [ "$CLOUDFLARE_RECORD_TYPE" = "AAAA" ]; then
-	IP_VERSION="-6"
+        IP_VERSION="-6"
 else
-	LOG_TIME=`date`
-	printf "$LOG_TIME: Invalid record type, CLOUDFLARE_RECORD_TYPE can only be A or AAAA.\n"
-	exit 2
+        LOG_TIME=`date`
+        printf "$LOG_TIME: Invalid record type, CLOUDFLARE_RECORD_TYPE can only be A or AAAA.\n"
+        exit 2
 fi
 
 if [ "$CLOUDFLARE_USER_MAIL" = "" ]; then
-	LOG_TIME=`date`
-	printf "$LOG_TIME: CLOUDFLARE_USER_MAIL is required.\n"
-	exit 2
+        LOG_TIME=`date`
+        printf "$LOG_TIME: CLOUDFLARE_USER_MAIL is required.\n"
+        exit 2
 fi
 
 if [ "$CLOUDFLARE_ZONE_NAME" = "" ]; then
-	LOG_TIME=`date`
-	printf "$LOG_TIME: CLOUDFLARE_ZONE_NAME is required.\n"
-	exit 2
+        LOG_TIME=`date`
+        printf "$LOG_TIME: CLOUDFLARE_ZONE_NAME is required.\n"
+        exit 2
 fi
 
 if [ "$OUTBOUND_INTERFACE" != "" ]; then
-	CURL_INTERFACE="--interface $OUTBOUND_INTERFACE"
+        CURL_INTERFACE="--interface $OUTBOUND_INTERFACE"
 else
-	CURL_INTERFACE=""
+        CURL_INTERFACE=""
 fi
 
 # Try to get IP using primary service;
 PUBLIC_IP=`curl $IP_VERSION $CURL_INTERFACE -s $PRIMARY_IP_API`
 # If primary service failed (empty result), try backup service;
 if [ -z "$PUBLIC_IP" ]; then
-	LOG_TIME=`date`
-	printf "$LOG_TIME: Primary IP service failed, trying backup service...\n"
-	PUBLIC_IP=`curl $IP_VERSION $CURL_INTERFACE -s $BACKUP_IP_API`
-    
-	if [ -z "$PUBLIC_IP" ]; then
-		LOG_TIME=`date`
-		printf "$LOG_TIME: Failed to get public IP address from both services.\n"
-		exit 1
-	fi
+        LOG_TIME=`date`
+        printf "$LOG_TIME: Primary IP service failed, trying backup service...\n"
+        PUBLIC_IP=`curl $IP_VERSION $CURL_INTERFACE -s $BACKUP_IP_API`
+        if [ -z "$PUBLIC_IP" ]; then
+                LOG_TIME=`date`
+                printf "$LOG_TIME: Failed to get public IP address from both services.\n"
+                exit 1
+        fi
 fi
 
 PUBLIC_IP_FILE=$workDir/.IP::$CLOUDFLARE_RECORD_TYPE::$CLOUDFLARE_RECORD_NAME.ddns
 
 if [ -f $PUBLIC_IP_FILE ]; then
-	OLD_PUBLIC_IP=`cat $PUBLIC_IP_FILE`
+        OLD_PUBLIC_IP=`cat $PUBLIC_IP_FILE`
 else
-	OLD_PUBLIC_IP=""
+        OLD_PUBLIC_IP=""
 fi
 
 if [ "$PUBLIC_IP" = "$OLD_PUBLIC_IP" ] && [ "$FORCE_UPDATE" = false ]; then
-	LOG_TIME=`date`
-	printf "$LOG_TIME: Public IP not changed, you can use -f to update anyway.\n"
-	exit 0
+        LOG_TIME=`date`
+        printf "$LOG_TIME: Public IP not changed, you can use -f to update anyway.\n"
+        exit 0
 fi
 
 CLOUDFLARE_ID_FILE=$workDir/.ID::$CLOUDFLARE_RECORD_TYPE::$CLOUDFLARE_RECORD_NAME.ddns
 
 if [ -f $CLOUDFLARE_ID_FILE ] && [ $(wc -l $CLOUDFLARE_ID_FILE | cut -d " " -f 1) == 4 ] \
-	&& [ "$(sed -n '3,1p' "$CLOUDFLARE_ID_FILE")" == "$CLOUDFLARE_ZONE_NAME" ] \
-	&& [ "$(sed -n '4,1p' "$CLOUDFLARE_ID_FILE")" == "$CLOUDFLARE_RECORD_NAME" ]; then
-		CLOUDFLARE_ZONE_ID=$(sed -n '1,1p' "$CLOUDFLARE_ID_FILE")
-		CLOUDFLARE_RECORD_ID=$(sed -n '2,1p' "$CLOUDFLARE_ID_FILE")
+        && [ "$(sed -n '3,1p' "$CLOUDFLARE_ID_FILE")" == "$CLOUDFLARE_ZONE_NAME" ] \
+        && [ "$(sed -n '4,1p' "$CLOUDFLARE_ID_FILE")" == "$CLOUDFLARE_RECORD_NAME" ]; then
+        CLOUDFLARE_ZONE_ID=$(sed -n '1,1p' "$CLOUDFLARE_ID_FILE")
+        CLOUDFLARE_RECORD_ID=$(sed -n '2,1p' "$CLOUDFLARE_ID_FILE")
 else
     if [ "$CLOUDFLARE_API_TOKEN" != "" ]; then
         CLOUDFLARE_API_KEY=$CLOUDFLARE_API_TOKEN
@@ -135,7 +134,7 @@ else
         -H "Content-Type: application/json" | grep -Po '(?<="id":")[^"]*' | head -1 )
         CLOUDFLARE_RECORD_ID=$(curl $CURL_INTERFACE -s \
         -X GET "https://api.cloudflare.com/client/v4/zones/$CLOUDFLARE_ZONE_ID/dns_records?name\
-	=$CLOUDFLARE_RECORD_NAME&type=$CLOUDFLARE_RECORD_TYPE" \
+=$CLOUDFLARE_RECORD_NAME&type=$CLOUDFLARE_RECORD_TYPE" \
         -H "X-Auth-Email: $CLOUDFLARE_USER_MAIL" \
         -H "Authorization: Bearer $CLOUDFLARE_API_TOKEN" \
         -H "Content-Type: application/json"  | grep -Po '(?<="id":")[^"]*' | head -1 )
@@ -152,7 +151,7 @@ else
         -H "Content-Type: application/json" | grep -Po '(?<="id":")[^"]*' | head -1 )
         CLOUDFLARE_RECORD_ID=$(curl $CURL_INTERFACE -s \
         -X GET "https://api.cloudflare.com/client/v4/zones/$CLOUDFLARE_ZONE_ID/dns_records?name\
-	=$CLOUDFLARE_RECORD_NAME&type=$CLOUDFLARE_RECORD_TYPE" \
+=$CLOUDFLARE_RECORD_NAME&type=$CLOUDFLARE_RECORD_TYPE" \
         -H "X-Auth-Email: $CLOUDFLARE_USER_MAIL" \
         -H "X-Auth-Key: $CLOUDFLARE_API_KEY" \
         -H "Content-Type: application/json"  | grep -Po '(?<="id":")[^"]*' | head -1 )
@@ -167,29 +166,29 @@ LOG_TIME=`date`
 printf "$LOG_TIME: Updating $CLOUDFLARE_RECORD_NAME to $PUBLIC_IP...\n"
 
 if [ "$CLOUDFLARE_API_TOKEN" != "" ]; then
-	CLOUDFLARE_API_RESPONSE=$(curl $CURL_INTERFACE -o /dev/null -s -w "%{http_code}\n" \
-		-X PUT "https://api.cloudflare.com/client/v4/zones/$CLOUDFLARE_ZONE_ID/dns_records/$CLOUDFLARE_RECORD_ID" \
-		-H "X-Auth-Email: $CLOUDFLARE_USER_MAIL" \
-		-H "Authorization: Bearer $CLOUDFLARE_API_TOKEN" \
-		-H "Content-Type: application/json" \
-		--data "{\"type\":\"$CLOUDFLARE_RECORD_TYPE\",\"name\":\"$CLOUDFLARE_RECORD_NAME\",\
-		\"content\":\"$PUBLIC_IP\", \"ttl\":120}")
+        CLOUDFLARE_API_RESPONSE=$(curl $CURL_INTERFACE -o /dev/null -s -w "%{http_code}\n" \
+        -X PUT "https://api.cloudflare.com/client/v4/zones/$CLOUDFLARE_ZONE_ID/dns_records/$CLOUDFLARE_RECORD_ID" \
+        -H "X-Auth-Email: $CLOUDFLARE_USER_MAIL" \
+        -H "Authorization: Bearer $CLOUDFLARE_API_TOKEN" \
+        -H "Content-Type: application/json" \
+        --data "{\"type\":\"$CLOUDFLARE_RECORD_TYPE\",\"name\":\"$CLOUDFLARE_RECORD_NAME\",\
+        \"content\":\"$PUBLIC_IP\", \"ttl\":120}")
 else
-	CLOUDFLARE_API_RESPONSE=$(curl $CURL_INTERFACE -o /dev/null -s -w "%{http_code}\n" \
-		-X PUT "https://api.cloudflare.com/client/v4/zones/$CLOUDFLARE_ZONE_ID/dns_records/$CLOUDFLARE_RECORD_ID" \
-		-H "X-Auth-Email: $CLOUDFLARE_USER_MAIL" \
-		-H "X-Auth-Key: $CLOUDFLARE_API_KEY" \
-		-H "Content-Type: application/json" \
-		--data "{\"type\":\"$CLOUDFLARE_RECORD_TYPE\",\"name\":\"$CLOUDFLARE_RECORD_NAME\",\
-		\"content\":\"$PUBLIC_IP\", \"ttl\":120}")
+        CLOUDFLARE_API_RESPONSE=$(curl $CURL_INTERFACE -o /dev/null -s -w "%{http_code}\n" \
+        -X PUT "https://api.cloudflare.com/client/v4/zones/$CLOUDFLARE_ZONE_ID/dns_records/$CLOUDFLARE_RECORD_ID" \
+        -H "X-Auth-Email: $CLOUDFLARE_USER_MAIL" \
+        -H "X-Auth-Key: $CLOUDFLARE_API_KEY" \
+        -H "Content-Type: application/json" \
+        --data "{\"type\":\"$CLOUDFLARE_RECORD_TYPE\",\"name\":\"$CLOUDFLARE_RECORD_NAME\",\
+        \"content\":\"$PUBLIC_IP\", \"ttl\":120}")
 fi
 
 if [[ "$CLOUDFLARE_API_RESPONSE" != 200 ]]; then
-	LOG_TIME=`date`
-	printf "$LOG_TIME: Failed to update record.\n"
-	exit 1
+        LOG_TIME=`date`
+        printf "$LOG_TIME: Failed to update record.\n"
+        exit 1
 else
-	LOG_TIME=`date`
-	printf "$LOG_TIME: $CLOUDFLARE_RECORD_NAME successfully updated to $PUBLIC_IP.\n"
-	printf '%s' "$PUBLIC_IP" > "$PUBLIC_IP_FILE"
+        LOG_TIME=`date`
+        printf "$LOG_TIME: $CLOUDFLARE_RECORD_NAME successfully updated to $PUBLIC_IP.\n"
+        printf '%s' "$PUBLIC_IP" > "$PUBLIC_IP_FILE"
 fi
